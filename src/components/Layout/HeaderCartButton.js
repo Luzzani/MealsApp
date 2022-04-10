52c433f46f08065bf23b2a397f2a870f17cda04e
Lucas Martin Luzzani <lucasluzzani@gmail.com>
2022-04-10 11:32:37 -0300

    Animacion al usar el boton add, que altera el tamaño del boton carrito de compras, usando useEffect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
   const cntx = useContext(CartContext);
 
-  const numberOfCartItems = cntx.items.reduce((currNumber, item) => {
+  const { items } = cntx;
+
+  const numberOfCartItems = items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    setBtnIsHighlighted(true);
+
+    return ()=>{clearTimeout(timer)};
+  }, [items]);
+
   return (
-    <button onClick={props.onShowHanlder} className={classes.button}>
+    <button onClick={props.onShowHanlder} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
